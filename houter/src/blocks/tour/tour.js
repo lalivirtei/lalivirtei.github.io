29c995f8da8921b5222dde8a992c7ee57094401a
Denis <lalivirtei@gmail.com>
2023-01-21 12:30:47 +0300

    Mobile fixes

--- houter/src/blocks/tour/tour.js
+++ houter/src/blocks/tour/tour.js
@@ -13,4 +13,26 @@ $(document).ready(function () {
         centeredSlides: true
     })
 
+    const $videoParent = $('.tour__video-wrapper');
+
+    if(window.matchMedia('(max-width: 1024px)').matches) {
+        let $iframe = $videoParent.find('.tour__iframe');
+        const src = $iframe.data('src');
+        $videoParent.addClass('tour__video-wrapper--active');
+        $iframe.attr('src', src);
+    }
+    // $('.tour__video-button-poster').on('click', function(e) {
+    //     e.preventDefault();
+    //     var poster = $(this);
+    //     var wrapper = poster.closest('.tour__video-wrapper');
+    //     videoPlay(wrapper);
+    // });
+    //
+    // function videoPlay(wrapper) {
+    //     var iframe = wrapper.find('.tour__iframe');
+    //     var src = iframe.data('src');
+    //     wrapper.addClass('tour__video-wrapper--active');
+    //     iframe.attr('src',src);
+    // }
+
 })
