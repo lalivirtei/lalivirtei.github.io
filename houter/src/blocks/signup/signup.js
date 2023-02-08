af00e708e99905270b14be027331b58d7753fdaf
Denis <lalivirtei@gmail.com>
2023-02-08 15:53:00 +0300

    signup ajax to formbold

--- houter/src/blocks/signup/signup.js
+++ houter/src/blocks/signup/signup.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     const $btnClose = $('.signup__button--close');
     let $body = $('.body');
 
-    $('.btn--signup').click(function() {
+    $('.btn--signup').click(function () {
         $signup.addClass('signup--open');
         $body.addClass('body--no-scroll');
     });
@@ -15,14 +15,29 @@ $(document).ready(function () {
         hideModal();
     });
 
-    $signup.click(function(e) {
-       if (!$(e.target).closest('.signup__container').length) {
-           hideModal();
-       }
+    $signup.click(function (e) {
+        if (!$(e.target).closest('.signup__container').length) {
+            hideModal();
+        }
     });
 
     function hideModal() {
         $signup.removeClass('signup--open');
         $body.removeClass('body--no-scroll');
     }
+
+    $signup.submit(function (e) {
+        e.preventDefault();
+        $.post(
+            'https://formbold.com/s/oJprW',
+            $('.signup').serialize(),
+            function (msg) {
+                $('.signup__container').text(msg.message);
+                setTimeout(function() {
+                    hideModal();
+                }, 1000)
+            },
+            'json'
+        )
+    })
 })
